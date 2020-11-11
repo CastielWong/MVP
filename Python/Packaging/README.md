b17346ad54528f241ce38333cc49e2d7661b5e2d
CastielWong <castielwongsysu@gmail.com>
2020-11-11 21:53:06 +1100

    Initialize Python Packaging project

---
+++ Python/Packaging/README.md
@@ -0,0 +1,23 @@
+
+- [Installation](#installation)
+    - [Local](#local)
+- [Reference](#reference)
+
+## Installation
+
+### Local
+
+To quickly test whether the configuration of "setup.py" is correct, simply run `pip install .` under this root
+project directory.
+
+Run codes below to invoke the module function:
+```py
+from caller import entry
+
+entry.main()
+```
+
+
+## Reference
+
+- Packaging Python Projects: https://packaging.python.org/tutorials/packaging-projects/
---
+++ Python/Packaging/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fr:
+    long_description = fr.read()
+
+required_packages = []
+with open("requirements.txt", "r") as fr:
+    for line in fr.read().splitlines():
+        # skip any comment/command line
+        if line.startswith("#") or "--" in line:
+            continue
+
+        required_packages.append(line)
+
+setuptools.setup(
+    name="packaging-xxx",  # it can be the same as the module
+    version="0.0.1",
+    author="Anonymous",
+    description="It's a MVP of Python packaging",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_required=required_packages,
+    extra_requirement={"develop": "virtualenv", "format": ["pre-commit"]},
+    url=None,
+    packages=setuptools.find_packages(),
+    python_required=">=3.6",
+)
