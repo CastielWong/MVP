ee1434e4d02282e22a013c65f7db70b2285bfbbf
CastielWong <castielwongsysu@gmail.com>
2023-04-29 16:04:45 +0800

    Set the calendar to visualize dates within the range

--- Python/Projects/Calendar/__main__.py
+++ Python/Projects/Calendar/__main__.py
@@ -35,8 +35,8 @@ def generate_dates_for_a_year(year: int) -> List[date]:
     return all_dates
 
 
-def highlight_dates_in_a_year(year: int, highlighted: List[date]):
-    r"""Highlight dates in a year.
+def highlight_dates_in_a_week(week: List[date], highlighted: List[date]):
+    r"""Highlight dates in a week.
 
     The way to highlight a number is via escaping characters like `\033[a;b;cm`, where
     `a;b;c` is optional.
@@ -48,15 +48,38 @@ def highlight_dates_in_a_year(year: int, highlighted: List[date]):
     - \033[m: reset text color to the default
 
     Args:
+        week: list of dates for a week
+        highlighted: dates
+    """
+    for date_obj in week:
+        # escape sequence that changes the color of the text that follows it
+        if date_obj in highlighted:
+            print(f"\033[1;32;40m{date_obj.day:2}\033[m", end=" ")
+        else:
+            print(f"{date_obj.day:2}", end=" ")
+    return
+
+
+def highlight_dates_in_a_year(year: int, highlighted: List[date]):
+    """Highlight dates in a year.
+
+    Args:
         year: the year integer
         highlighted: dates
     """
     cal = calendar.Calendar()
     year_calendar: List[List[Any]] = cal.yeardatescalendar(year=year, width=3)
 
+    earliest = min(highlighted)
+    latest = max(highlighted)
+
     # Create a calendar with the highlighted dates
     for quarter_calendar in year_calendar:
         for month_calendar in quarter_calendar:
+            # no need to output months out of the range
+            if month_calendar[-1][-1] < earliest or month_calendar[0][0] > latest:
+                continue
+
             # first element of the second list must be for the month
             month_ = month_calendar[1][0].month
 
@@ -68,12 +91,8 @@ def highlight_dates_in_a_year(year: int, highlighted: List[date]):
                 if week_calendar[0].month != month_:
                     continue
 
-                for date_obj in week_calendar:
-                    # escape sequence that changes the color of the text that follows it
-                    if date_obj in highlighted:
-                        print(f"\033[1;32;40m{date_obj.day:2}\033[m", end=" ")
-                    else:
-                        print(f"{date_obj.day:2}", end=" ")
+                highlight_dates_in_a_week(week=week_calendar, highlighted=highlighted)
+
                 # output newline after a week
                 print()
 
@@ -103,4 +122,4 @@ def main(file_name: str, start_year: int, end_year: int):
 
 
 if __name__ == "__main__":
-    main("check.yaml", 2023, 2024)
+    main("check.yaml", 2010, 2024)
