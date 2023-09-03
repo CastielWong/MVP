e5f55e243d8e71aa8800e21dfe1a6513b70d5575
CastielWong <castielwongsysu@gmail.com>
2023-09-03 15:00:36 +0800

    Add exception and side_effect examples

--- Python/Projects/0_Example/tests/unit/test_connection.py
+++ Python/Projects/0_Example/tests/unit/test_connection.py
@@ -4,6 +4,7 @@
 
 from pytest_mock import MockerFixture
 from core.connection import SqlServer
+import pytest
 
 DUMMY_CONFIG = {
     "server": "localhost",
@@ -11,6 +12,7 @@ DUMMY_CONFIG = {
     "username": "sa",
     "password": "dummy",
 }
+DUMMY_QUERY = "SELECT * FROM dummy"
 
 
 def test_context_manager(mocker: MockerFixture):
@@ -25,9 +27,22 @@ def test_context_manager(mocker: MockerFixture):
     return
 
 
-def test_run_script(mocker: MockerFixture):
-    """Test running query."""
-    query = "SELECT * FROM dummy"
+def test_run_script__exception(mocker: MockerFixture):
+    """Test run_script() when there is RuntimeError."""
+    mocker.patch("pyodbc.connect", return_values=None)
+
+    with pytest.raises(RuntimeError) as exc_info:
+        sql_server = SqlServer(**DUMMY_CONFIG)
+        sql_server.run_script(DUMMY_QUERY)
+
+    # note the exception message can only be checked after but not in the context
+    assert str(exc_info.value) == "The cursor of connection shouldn't be None"
+
+    return
+
+
+def test_run_script__called_with(mocker: MockerFixture):
+    """Test run_script() to called with query specified."""
     expected = ["a", "b", "c"]
 
     mocker.patch("pyodbc.connect", return_values=mocker.MagicMock())
@@ -36,11 +51,31 @@ def test_run_script(mocker: MockerFixture):
         patcher_fetchall = mocker.patch.object(conn.cursor.__enter__(), "fetchall")
         patcher_fetchall.return_value = expected
 
-        actual = conn.run_script(query)
+        actual = conn.run_script(DUMMY_QUERY)
 
         # assert the query is called inside `execute()`
-        conn.cursor.__enter__().execute.assert_called_with(query)
+        conn.cursor.__enter__().execute.assert_called_with(DUMMY_QUERY)
 
         assert actual == expected
+
+    return
+
+
+def test_run_script__side_effect(mocker: MockerFixture):
+    """Test run_script() for multiple time runs."""
+    # simulate results returned by `cursor.execute()`
+    results = [[("1st result",)], [("2nd",)], [("the third",)]]
+
+    mocker.patch("pyodbc.connect", return_values=mocker.MagicMock())
+
+    with SqlServer(**DUMMY_CONFIG) as conn:  # type: ignore
+        # mock all results returned
+        patcher_fetchall = mocker.patch.object(conn.cursor.__enter__(), "fetchall")
+        patcher_fetchall.side_effect = results
+
+        # assert the result for each run
+        for expected in results:
+            actual = conn.run_script(DUMMY_QUERY)
+            assert actual == expected
 
     return
