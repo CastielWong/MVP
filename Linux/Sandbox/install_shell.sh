ecef21fbe8746a32a1e906edac7aeb1a87a05ec4
CastielWong <castielwongsysu@gmail.com>
2025-04-15 18:18:41 +0800

    Update the script to initialize zsh shell; reorganize tool installation for better portability

--- Linux/Sandbox/install_shell.sh
+++ Linux/Sandbox/install_shell.sh
@@ -19,20 +19,77 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # -----------------------------------------------------------------------------
 # add themes in oh-my-zsh
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+ZSH_THEMES=${ZSH_CUSTOM}/themes
 
-# theme: powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-# # MANUAL STEP: config the theme
-# p10k configure
+download_theme() {
+    local theme_name=$1
+    local repo_url=$2
 
-# theme: spaceship
-git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt
-ln -s "${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM}/themes/spaceship.zsh-theme"
+    proj_theme=${ZSH_THEMES}/${theme_name}
+    if [[ ! -d "${proj_theme}" ]]; then
+        echo "Downloading theme: '${theme_name}'"
+        git clone --depth=1 "${repo_url}" "${proj_theme}" || echo "Failed to download '${theme_name}'"
+    else
+        echo "Theme '${theme_name}' is available already"
+    fi
+}
 
-# theme: agnoster
-git clone --depth=1 https://github.com/agnoster/agnoster-zsh-theme.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/agnoster
-ln -s "${ZSH_CUSTOM}/themes/agnoster/agnoster.zsh-theme" "${ZSH_CUSTOM}/themes/agnoster.zsh-theme"
+link_theme() {
+    local theme_name=$1
+    local name_linked=$2
+
+    source_theme="${ZSH_THEMES}/${theme_name}/${name_linked}.zsh-theme"
+    target_theme="${ZSH_THEMES}/${name_linked}.zsh-theme"
+    if [[ ! -f ${target_theme} ]]; then
+        echo "Linking to '${target_theme}'"
+        ln -s "${source_theme}" "${target_theme}" || echo "Failed to link '${theme_name}'"
+    else
+        echo "'${target_theme}' was linked before"
+    fi
+}
+
+
+themes=(
+    # run `p10k configure` to configure
+    "powerlevel10k https://github.com/romkatv/powerlevel10k.git"
+    "spaceship-prompt https://github.com/spaceship-prompt/spaceship-prompt.git"
+    "agnoster https://github.com/agnoster/agnoster-zsh-theme.git"
+)
+
+
+themes_to_link=(
+    "spaceship-prompt spaceship"
+    "agnoster agnoster"
+)
+
+for theme in "${themes[@]}"; do
+    echo "--------------------------------------------------------------------"
+    read -r theme_name repo_url <<< "${theme}"
+    download_theme "${theme_name}" "${repo_url}"
+done
+
+for theme in "${themes_to_link[@]}"; do
+    echo "--------------------------------------------------------------------"
+    read -r theme_name name_linked <<< "${theme}"
+    link_theme "${theme_name}" "${name_linked}"
+done
 
 # -----------------------------------------------------------------------------
 # add plugins in oh-my-zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+plugins=(
+    "zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions"
+)
+
+for plugin in "${plugins[@]}"; do
+    echo "--------------------------------------------------------------------"
+    read -r plugin_name repo_url <<< "${plugin}"
+
+    plugin_folder=${ZSH_CUSTOM}/plugins/${plugin_name}
+    if [[ ! -d ${plugin_folder} ]]; then
+        echo "Downloading plugin '${target_theme}'"
+        git clone "${repo_url}" "${plugin_folder}"
+    else
+        echo "Plugin '${plugin}' is available alread"
+    fi
+done
--- Linux/Sandbox/install_tools.sh
+++ Linux/Sandbox/install_tools.sh
@@ -5,26 +5,9 @@
 # export no_proxy="*.com,vdi*,sso.com*""
 
 # =============================================================================
+# place all tools inside a dedicated place
 export WORKING_DIRECTORY="tools"
 
-# set environment variables
-export TERRAFORM_VERSION="1.9.5"
-export K9S_VERSION="v0.50.2"
-export KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-export HELM_SCRIPT="install_helm.sh"
-
-export URL_AWSCLI="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
-export URL_TERRAFORM="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_386.zip"
-export URL_KUBECTL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
-export URL_HELM="https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
-export URL_K9S="https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_amd64.tar.gz"
-
-export UNZIP_AWSCLI="unzip_aws"
-export UNZIP_TERRAFORM="unzip_terraform"
-export UNZIP_K9S="unzip_k9s"
-
-# -----------------------------------------------------------------------------
-# place all tools inside a dedicated place
 if [[ ! -d "${WORKING_DIRECTORY}" ]]; then
   # Create the directory
   mkdir "${WORKING_DIRECTORY}"
@@ -35,18 +18,57 @@ fi
 
 cd "${WORKING_DIRECTORY}/"
 
-# -----------------------------------------------------------------------------
-# Downloading
+# *****************************************************************************
+# set environment variables
+# AWS CLI
+export URL_AWSCLI="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+export UNZIP_AWSCLI="unzip_aws"
+
+# Terraform
+export TERRAFORM_VERSION="1.9.5"
+export URL_TERRAFORM="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_386.zip"
+export UNZIP_TERRAFORM="unzip_terraform"
+
+# Kubernetes
+export KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+export URL_KUBECTL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+
+# Helm
+export URL_HELM="https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
+export HELM_SCRIPT="install_helm.sh"
+
+# K9S
+export K9S_VERSION="v0.50.2"
+export URL_K9S="https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_amd64.tar.gz"
+export UNZIP_K9S="unzip_k9s"
+
+# Vault
+export VAULT_VERSION="1.19.2"
+export URL_VAULT="https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip"
+export UNZIP_VAULT="unzip_vault"
+
+# *****************************************************************************
+# Downloading -> Installing
 # AWS CLI
 if [[ ! -f $(basename ${URL_AWSCLI}) ]]; then
   curl ${URL_AWSCLI} -o $(basename ${URL_AWSCLI})
 fi
 
+if ! command -v aws > /dev/null 2>&1; then
+  unzip $(basename ${URL_AWSCLI}) -d ${UNZIP_AWSCLI}
+  sudo ./${UNZIP_AWSCLI}/aws/install
+fi
+# -----------------------------------------------------------------------------
 # Terraform
 if [[ ! -f $(basename ${URL_TERRAFORM}) ]]; then
   curl -LO ${URL_TERRAFORM}
 fi
 
+if ! command -v terraform > /dev/null 2>&1; then
+  unzip $(basename ${URL_TERRAFORM}) -d ${UNZIP_TERRAFORM}
+  sudo cp ${UNZIP_TERRAFORM}/terraform /usr/local/bin/
+fi
+# -----------------------------------------------------------------------------
 # kubectl
 if [[ ! -f $(basename ${URL_KUBECTL}) ]]; then
   curl -LO ${URL_KUBECTL}
@@ -56,55 +78,48 @@ if [[ ! -f $(basename ${URL_KUBECTL}) ]]; then
   echo "LOGGING: $(cat kubectl.sha256)  kubectl" | sha256sum --check
 fi
 
+if ! command -v kubectl > /dev/null 2>&1; then
+  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+fi
+# -----------------------------------------------------------------------------
 # Helm
 if [[ ! -f "${HELM_SCRIPT}" ]]; then
-  curl -fsSL -o ${HELM_SCRIPT} ${URL_HELM}
+  curl -fSL -o ${HELM_SCRIPT} ${URL_HELM}
 fi
 
+if ! command -v helm > /dev/null 2>&1; then
+  chmod 700 ${HELM_SCRIPT}
+  ./${HELM_SCRIPT}
+fi
+# -----------------------------------------------------------------------------
 # K9S
 if [[ ! -f $(basename ${URL_k9s}) ]]; then
   curl -LO ${URL_K9S} -o $(basename ${URL_K9S})
 fi
 
-# -----------------------------------------------------------------------------
-
-# Installing
-# AWS CLI
-if ! command -v aws > /dev/null 2>&1; then
-  unzip $(basename ${URL_AWSCLI}) -d ${UNZIP_AWSCLI}
-  sudo ./${UNZIP_AWSCLI}/aws/install
-fi
-
-# Terraform
-if ! command -v terraform > /dev/null 2>&1; then
-  unzip $(basename ${URL_TERRAFORM}) -d ${UNZIP_TERRAFORM}
-  sudo cp ${UNZIP_TERRAFORM}/terraform /usr/local/bin/
-fi
-
-# kubectl
-if ! command -v kubectl > /dev/null 2>&1; then
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-fi
-
-# Helm
-if ! command -v helm > /dev/null 2>&1; then
-  chmod 700 ${HELM_SCRIPT}
-  ./${HELM_SCRIPT}
-fi
-
-# K9S
 if ! command -v k9s > /dev/null 2>&1; then
   mkdir ${UNZIP_K9S}; tar -xzvf $(basename ${URL_K9S}) -C ${UNZIP_K9S}
   sudo mv ${UNZIP_K9S}/k9s /usr/local/bin/
 fi
-
 # -----------------------------------------------------------------------------
+# Vault
+if [[ ! -f $(basename ${URL_VAULT}) ]]; then
+  curl -fSLO ${URL_VAULT}
+fi
 
+if ! command -v vault > /dev/null 2>&1; then
+  mkdir ${UNZIP_VAULT}; unzip $(basename ${URL_VAULT}) -d ${UNZIP_VAULT}
+
+  sudo mv ./${UNZIP_VAULT}/vault /usr/local/bin/
+fi
+
+# *****************************************************************************
 # Cleaning
 direcotires=(
   "${UNZIP_AWSCLI}"
   "${UNZIP_TERRAFORM}"
   "${UNZIP_K9S}"
+  "${UNZIP_VAULT}"
 )
 
 for dir in "${direcotires[@]}"; do
@@ -120,3 +135,12 @@ done
 rm "$(basename ${URL_KUBECTL}).sha256"
 
 cd -
+
+# *****************************************************************************
+# Verifying
+aws --version
+terraform version
+kubectl version
+helm version
+k9s version
+vault version
